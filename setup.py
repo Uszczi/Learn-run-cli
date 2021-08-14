d22fc7ed5e4684a5f84252371e2de73c9d426596
Mateusz Papuga <mateuszpapuga24@gmail.com>
2021-08-14 10:58:43 +0200

    flat structure

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="learn-run-cli",
     version="1.0",
     packages=["lrc"],
-    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
             "learn-run-cli = lrc.main:main"
